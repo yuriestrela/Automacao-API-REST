74230ca1ba3ed929469996a6cfdaa3f5219b2862
Suporte TI <suporte@CARMELCHARME.DOM>
2025-05-27 20:38:28 -0300

    teste da linguiça

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,14 +2,14 @@ describe('Login', () => {
 
     beforeEach(() => {
         cy.visit('https://automationpratice.com.br/login');
-        cy.get('a[href="/login"]').first().click();
+        // cy.get('a[href="/login"]').first().click();
         
     });    
     it('Login com sucesso', () => {
 
         cy.get('#user').type('yuri@qa.com');
         cy.get('#password').type('123456');
-        cy.get('#btnLogin').click();
+        cy.get('#btnLoginnnnn').click();
 
         // cy.get('#swal2-title').should('be.visible');
         cy.get('#swal2-title').should('have.text', 'Login realizado');
